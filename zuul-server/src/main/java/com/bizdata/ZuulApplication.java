7ed135131410f2f1e4467443370290f3f35721b2
sdevil507 <sdevil507@163.com>
2017-05-02 14:36:50 +0800

    去除未使用到导入包

--- zuul-server/src/main/java/com/bizdata/ZuulApplication.java
+++ zuul-server/src/main/java/com/bizdata/ZuulApplication.java
@@ -1,16 +1,14 @@
 package com.bizdata;
 
+import com.bizdata.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-
-import com.bizdata.filter.AccessFilter;
 
 @EnableZuulProxy
 @EnableFeignClients
