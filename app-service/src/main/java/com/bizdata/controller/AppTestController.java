4fdb5578016ed296178d68fd81f62ecd58536945
sdevil507 <sdevil507@163.com>
2017-05-02 16:52:58 +0800

    新增app-service工程.用于快速构建

---
+++ app-service/src/main/java/com/bizdata/controller/AppTestController.java
@@ -0,0 +1,28 @@
+package com.bizdata.controller;
+
+import com.bizdata.token.TokenServiceFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试Controller
+ * <p>
+ * Created by sdevil507 on 2017/5/2.
+ */
+@RestController
+public class AppTestController {
+    @Autowired
+    private TokenServiceFeign tokenServiceFeign;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public void appLogin() {
+        tokenServiceFeign.createToken("app", "123456");
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public void appTest() {
+        System.out.println("访问到该接口!");
+    }
+}
