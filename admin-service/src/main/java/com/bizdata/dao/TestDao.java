445d6f2fa17c926a7dc51f37a074da805609bd43
sdevil507 <sdevil507@163.com>
2017-05-09 14:20:04 +0800

    去除TestDao中警告

--- admin-service/src/main/java/com/bizdata/dao/TestDao.java
+++ admin-service/src/main/java/com/bizdata/dao/TestDao.java
@@ -2,10 +2,10 @@ package com.bizdata.dao;
 
 import com.bizdata.entity.Test;
 import com.bizdata.jpa.base.JpaBaseRepository;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 
 /**
+ * 测试Dao
+ * <p>
  * Created by sdevil507 on 2017/4/14.
  */
 public interface TestDao extends JpaBaseRepository<Test, String> {
