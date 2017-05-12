e07b5a979915f50795502752d2df4cc0f3bbb442
sdevil507 <sdevil507@163.com>
2017-05-12 14:41:27 +0800

    新增用户组织机构Service方法

--- admin-service/src/main/java/com/bizdata/service/UserOrganizationService.java
+++ admin-service/src/main/java/com/bizdata/service/UserOrganizationService.java
@@ -1,9 +1,19 @@
 package com.bizdata.service;
 
+import java.util.List;
+
 /**
  * 用户组织机构关系Service
  * <p>
  * Created by sdevil507 on 2017/5/9.
  */
 public interface UserOrganizationService {
+
+    /**
+     * 递归查询出该组织机构下所有用户id
+     *
+     * @param organizationID 组织机构ID
+     * @return List<String>
+     */
+    List<String> findAllUserIDsRecursion(String organizationID);
 }
--- admin-service/src/main/java/com/bizdata/service/impl/UserOrganizationServiceImpl.java
+++ admin-service/src/main/java/com/bizdata/service/impl/UserOrganizationServiceImpl.java
@@ -1,7 +1,15 @@
 package com.bizdata.service.impl;
 
+import com.bizdata.dao.OrganizationDao;
+import com.bizdata.dao.UserOrganizationDao;
+import com.bizdata.po.Organization;
+import com.bizdata.po.UserOrganizationRelation;
 import com.bizdata.service.UserOrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 用户组织机构关系Service实现
@@ -10,4 +18,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserOrganizationServiceImpl implements UserOrganizationService {
+
+    @Autowired
+    private OrganizationDao organizationDao;
+
+    @Autowired
+    private UserOrganizationDao userOrganizationDao;
+
+    /**
+     * 根据组织机构ID递归查询出所有该节点及子节点下的用户ID列表
+     *
+     * @param organizationID 组织机构ID
+     * @return 用户ID列表
+     */
+    @Override
+    public List<String> findAllUserIDsRecursion(String organizationID) {
+        List<String> organizationIDs = new ArrayList<>();
+        organizationIDs.add(organizationID);
+        organizationIDs.addAll(getNextLevel(organizationDao.findByParent(organizationID)));
+        List<String> userIDs = new ArrayList<>();
+        List<UserOrganizationRelation> userOrganizationRelations = userOrganizationDao.findByOrganizationIDIn(organizationIDs);
+        for (UserOrganizationRelation userOrganizationRelation : userOrganizationRelations) {
+            userIDs.add(userOrganizationRelation.getUserID());
+        }
+        return userIDs;
+    }
+
+    /**
+     * 递归获取子节点的组织机构ID
+     *
+     * @param list 组织机构列表
+     * @return 组织机构列表ID
+     */
+    private List<String> getNextLevel(List<Organization> list) {
+        List<String> temp_list = new ArrayList<>();
+        for (Organization organization : list) {
+            temp_list.add(organization.getId());
+            temp_list.addAll(getNextLevel(organizationDao.findByParent(organization.getId())));
+        }
+        return temp_list;
+    }
 }
