99c7704902efcc1440a10108082962f5e3cf4564
sdevil507 <sdevil507@163.com>
2017-05-12 17:25:15 +0800

    新增用户角色关系绑定与解除Controller方法

--- admin-service/src/main/java/com/bizdata/controller/UserRoleRelationController.java
+++ admin-service/src/main/java/com/bizdata/controller/UserRoleRelationController.java
@@ -26,14 +26,51 @@ public class UserRoleRelationController {
      * @param userIDs 用户ID列表
      *                return ResultStateVO
      */
-    @RequestMapping(value = "/userRoleRelation/build", method = RequestMethod.POST)
-    public ResultStateVO build(String roleID, String[] userIDs) {
+//    @RequestMapping(value = "/userRoleRelation/build", method = RequestMethod.POST)
+//    public ResultStateVO build(String roleID, String[] userIDs) {
+//        ResultStateVO resultStateVO;
+//        if (userRoleRelationService.build(roleID, userIDs)) {
+//            resultStateVO = ResultStateUtil.create(0, "角色绑定成功!");
+//        } else {
+//            resultStateVO = ResultStateUtil.create(1, "角色绑定失败!");
+//        }
+//        return resultStateVO;
+//    }
+
+    /**
+     * 新增用户角色关系
+     *
+     * @param roleID  角色ID
+     * @param userIDs 用户ID列表
+     * @return ResultStateVO
+     */
+    @RequestMapping(value = "/userRoleRelation/save", method = RequestMethod.POST)
+    public ResultStateVO save(String roleID, String[] userIDs) {
         ResultStateVO resultStateVO;
-        if (userRoleRelationService.build(roleID, userIDs)) {
-            resultStateVO = ResultStateUtil.create(0, "角色绑定成功!");
+        if (userRoleRelationService.save(roleID, userIDs)) {
+            resultStateVO = ResultStateUtil.create(0, "用户角色关系新增成功!");
         } else {
-            resultStateVO = ResultStateUtil.create(1, "角色绑定失败!");
+            resultStateVO = ResultStateUtil.create(1, "用户角色关系新增失败!");
         }
         return resultStateVO;
     }
+
+    /**
+     * 删除用户角色关系
+     *
+     * @param roleID  角色ID
+     * @param userIDs 用户ID列表
+     * @return ResultStateVO
+     */
+    @RequestMapping(value = "/userRoleRelation/delete", method = RequestMethod.POST)
+    public ResultStateVO delete(String roleID, String[] userIDs) {
+        ResultStateVO resultStateVO;
+        if (userRoleRelationService.delete(roleID, userIDs)) {
+            resultStateVO = ResultStateUtil.create(0, "用户角色关系解除成功!");
+        } else {
+            resultStateVO = ResultStateUtil.create(1, "用户角色关系解除失败!");
+        }
+        return resultStateVO;
+    }
+
 }
