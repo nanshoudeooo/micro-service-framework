169ccafebc12295765c6fb0a992dbb73ff4c5e8a
sdevil507 <sdevil507@163.com>
2017-05-12 14:40:55 +0800

    修改角色相关方法名

--- admin-service/src/main/java/com/bizdata/service/RoleService.java
+++ admin-service/src/main/java/com/bizdata/service/RoleService.java
@@ -1,12 +1,13 @@
 package com.bizdata.service;
 
+import com.bizdata.controller.user.vo.out.OutUserVO;
 import com.bizdata.po.Role;
 import com.bizdata.jpa.vo.JpaPageParamVO;
 import com.bizdata.jpa.vo.JpaSortParamVO;
 import com.bizdata.result.ResultStateVO;
-import com.bizdata.vo.role.RoleCreateParamVO;
-import com.bizdata.vo.role.RoleDeleteParamVO;
-import com.bizdata.vo.role.RoleUpdateParamVO;
+import com.bizdata.controller.role.vo.in.InSaveVO;
+import com.bizdata.controller.role.vo.in.InDeleteVO;
+import com.bizdata.controller.role.vo.in.InUpdateVO;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
@@ -21,26 +22,26 @@ public interface RoleService {
     /**
      * 角色创建
      *
-     * @param roleCreateParamVO 角色入参VO
+     * @param inSaveVO 角色入参VO
      * @return CodeState类型执行反馈
      */
-    ResultStateVO create(RoleCreateParamVO roleCreateParamVO);
+    ResultStateVO save(InSaveVO inSaveVO);
 
     /**
      * 角色更新
      *
-     * @param roleUpdateParamVO 入参VO
+     * @param inUpdateVO 入参VO
      * @return CodeState类型执行反馈
      */
-    ResultStateVO update(RoleUpdateParamVO roleUpdateParamVO);
+    ResultStateVO update(InUpdateVO inUpdateVO);
 
     /**
      * 角色删除
      *
-     * @param roleDeleteParamVO 入参VO
+     * @param inDeleteVO 入参VO
      * @return boolean执行反馈
      */
-    ResultStateVO delete(RoleDeleteParamVO roleDeleteParamVO);
+    ResultStateVO delete(InDeleteVO inDeleteVO);
 
     /**
      * 分页查询所有
@@ -49,7 +50,7 @@ public interface RoleService {
      * @param jpaSortParamVO 排序入参VO
      * @return Page<Role>
      */
-    Page<Role> findAll(JpaPageParamVO jpaPageParamVO, JpaSortParamVO jpaSortParamVO);
+    Page<Role> list(JpaPageParamVO jpaPageParamVO, JpaSortParamVO jpaSortParamVO);
 
     /**
      * 根据ID获取所有ROLE列表
@@ -57,7 +58,7 @@ public interface RoleService {
      * @param userID 用户ID
      * @return List<Role>
      */
-    List<Role> findAllByUserID(String userID);
+    List<Role> listByUserID(String userID);
 
     /**
      * 根据角色ID获取角色信息
@@ -65,5 +66,23 @@ public interface RoleService {
      * @param roleID 角色ID
      * @return Role
      */
-    Role findOne(String roleID);
+    Role getByID(String roleID);
+
+    /**
+     * 根据角色ID和组织机构ID条件进行用户筛选(授权)
+     *
+     * @param roleID         角色ID
+     * @param organizationID 组织机构ID
+     * @return List<OutUserVO>
+     */
+    List<OutUserVO> listAuthorizedUsersByRoleIDAndOrganizationID(String roleID, String organizationID);
+
+    /**
+     * 根据角色ID和组织机构ID条件进行用户筛选(未授权)
+     *
+     * @param roleID         角色ID
+     * @param organizationID 组织机构ID
+     * @return List<OutUserVO>
+     */
+    List<OutUserVO> listUnauthorizedUsersByRoleIDAndOrganizationID(String roleID, String organizationID);
 }
--- admin-service/src/main/java/com/bizdata/service/impl/RoleServiceImpl.java
+++ admin-service/src/main/java/com/bizdata/service/impl/RoleServiceImpl.java
@@ -1,8 +1,13 @@
 package com.bizdata.service.impl;
 
+import com.bizdata.controller.user.vo.out.OutUserVO;
 import com.bizdata.dao.RoleDao;
+import com.bizdata.dao.UserDao;
+import com.bizdata.dao.UserOrganizationDao;
 import com.bizdata.dao.UserRoleRelationDao;
+import com.bizdata.jpa.vo.JpaListPO2VO;
 import com.bizdata.po.Role;
+import com.bizdata.po.User;
 import com.bizdata.po.UserRoleRelation;
 import com.bizdata.extend.BeanCopyUtil;
 import com.bizdata.jpa.vo.JpaPageParamVO;
@@ -10,15 +15,17 @@ import com.bizdata.jpa.vo.JpaSortParamVO;
 import com.bizdata.result.ResultStateUtil;
 import com.bizdata.result.ResultStateVO;
 import com.bizdata.service.RoleService;
-import com.bizdata.vo.role.RoleCreateParamVO;
-import com.bizdata.vo.role.RoleDeleteParamVO;
-import com.bizdata.vo.role.RoleUpdateParamVO;
+import com.bizdata.service.UserOrganizationService;
+import com.bizdata.controller.role.vo.in.InSaveVO;
+import com.bizdata.controller.role.vo.in.InDeleteVO;
+import com.bizdata.controller.role.vo.in.InUpdateVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,19 +44,28 @@ public class RoleServiceImpl implements RoleService {
     private RoleDao roleDao;
 
     @Autowired
+    private UserDao userDao;
+
+    @Autowired
     private UserRoleRelationDao userRoleRelationDao;
 
+    @Autowired
+    private UserOrganizationDao userOrganizationDao;
+
+    @Autowired
+    private UserOrganizationService userOrganizationService;
+
     @Override
-    public ResultStateVO create(RoleCreateParamVO roleCreateParamVO) {
+    public ResultStateVO save(InSaveVO inSaveVO) {
         ResultStateVO resultStateVO;
         try {
             //判断角色名称是否存在
-            Role tempRole = roleDao.findByRolename(roleCreateParamVO.getRolename());
-            if (!checkRolenameNotExist(roleCreateParamVO.getRolename())) {
+            Role tempRole = roleDao.findByRolename(inSaveVO.getRolename());
+            if (!checkRolenameNotExist(inSaveVO.getRolename())) {
                 resultStateVO = ResultStateUtil.create(1, "角色创建失败,请确认角色名称不重复!");
             } else {
                 Role rolePO = new Role();
-                BeanUtils.copyProperties(roleCreateParamVO, rolePO);
+                BeanUtils.copyProperties(inSaveVO, rolePO);
                 roleDao.save(rolePO);
                 resultStateVO = ResultStateUtil.create(0, "角色创建成功!");
             }
@@ -61,11 +77,11 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public ResultStateVO update(RoleUpdateParamVO roleUpdateParamVO) {
+    public ResultStateVO update(InUpdateVO inUpdateVO) {
         ResultStateVO resultStateVO;
         try {
             Role rolePO = new Role();
-            BeanCopyUtil.copyProperties(roleUpdateParamVO, rolePO);
+            BeanCopyUtil.copyProperties(inUpdateVO, rolePO);
             roleDao.save(rolePO);
             resultStateVO = ResultStateUtil.create(0, "角色更新成功!");
         } catch (Exception e) {
@@ -76,14 +92,14 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public ResultStateVO delete(RoleDeleteParamVO roleDeleteParamVO) {
+    public ResultStateVO delete(InDeleteVO inDeleteVO) {
         ResultStateVO resultStateVO;
         try {
-            if (checkBuiltInRole(roleDeleteParamVO.getId())) {
+            if (checkBuiltInRole(inDeleteVO.getId())) {
                 //如果是系统内置角色
                 resultStateVO = ResultStateUtil.create(2, "无法删除系统内置角色!");
             } else {
-                roleDao.delete(roleDeleteParamVO.getId());
+                roleDao.delete(inDeleteVO.getId());
                 resultStateVO = ResultStateUtil.create(0, "角色删除成功!");
             }
         } catch (Exception e) {
@@ -94,7 +110,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public Page<Role> findAll(JpaPageParamVO jpaPageParamVO, JpaSortParamVO jpaSortParamVO) {
+    public Page<Role> list(JpaPageParamVO jpaPageParamVO, JpaSortParamVO jpaSortParamVO) {
         Page<Role> page = null;
         try {
             page = roleDao.findAll(jpaPageParamVO.getPageable(jpaSortParamVO.getSort()));
@@ -105,7 +121,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public List<Role> findAllByUserID(String userID) {
+    public List<Role> listByUserID(String userID) {
         //获取用户角色关系列表
         List<UserRoleRelation> userRoleRelations = userRoleRelationDao.findByUserID(userID);
         List<Role> roles = new ArrayList<>();
@@ -116,8 +132,96 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public Role findOne(String roleID) {
+    public Role getByID(String roleID) {
         return roleDao.findOne(roleID);
+    }
+
+    @Override
+    public List<OutUserVO> listAuthorizedUsersByRoleIDAndOrganizationID(String roleID, String organizationID) {
+        List<OutUserVO> outUserVOs;
+        try {
+            //根据角色ID获取该角色下用户列表
+            List<UserRoleRelation> userRoleRelations = userRoleRelationDao.findByRoleID(roleID);
+            List<String> userRoleIds = new ArrayList<>();
+            for (UserRoleRelation userRoleRelation : userRoleRelations) {
+                userRoleIds.add(userRoleRelation.getUserID());
+            }
+            if (StringUtils.isEmpty(organizationID)) {
+                //如果未传递组织机构ID条件,直接返回该角色下用户
+                List<User> users = userDao.findByIdIn(userRoleIds);
+                outUserVOs = JpaListPO2VO.convert(users, OutUserVO.class);
+            } else {
+                //如果包含组织机构ID条件
+                //首先获取该组织机构下用户ID
+                List<String> userOrganizationIds = userOrganizationService.findAllUserIDsRecursion(organizationID);
+
+                //既属于角色又属于组织机构条件下的id列表
+                List<String> intersectionIds = new ArrayList<>();
+                for (String userOrganizationId : userOrganizationIds) {
+                    if (userRoleIds.contains(userOrganizationId)) {
+                        intersectionIds.add(userOrganizationId);
+                    }
+                }
+                //根据交集ID列表返回用户列表
+                List<User> users = userDao.findByIdIn(intersectionIds);
+                outUserVOs = JpaListPO2VO.convert(users, OutUserVO.class);
+            }
+        } catch (Exception e) {
+            outUserVOs = null;
+            logger.error("获取授权用户列表失败!", e);
+        }
+        return outUserVOs;
+    }
+
+    @Override
+    public List<OutUserVO> listUnauthorizedUsersByRoleIDAndOrganizationID(String roleID, String organizationID) {
+        List<OutUserVO> outUserVOs;
+        try {
+            //根据角色ID获取该角色下用户列表
+            List<UserRoleRelation> userRoleRelations = userRoleRelationDao.findByRoleID(roleID);
+            List<String> userRoleIds = new ArrayList<>();
+            for (UserRoleRelation userRoleRelation : userRoleRelations) {
+                userRoleIds.add(userRoleRelation.getUserID());
+            }
+
+            boolean state = true;
+            if (0 == userRoleIds.size()) {
+                // 该role下没有用户,则查询全部
+                state = false;
+            }
+
+            if (StringUtils.isEmpty(organizationID)) {
+                //如果未传递组织机构ID条件,直接返回该角色下未授权用户
+                List<User> users = new ArrayList<>();
+                if (state) {
+                    users = userDao.findByIdNotIn(userRoleIds);
+                } else {
+                    users = userDao.findAll();
+                }
+                outUserVOs = JpaListPO2VO.convert(users, OutUserVO.class);
+            } else {
+                //如果包含组织机构ID条件
+                //首先获取该组织机构下用户ID
+                List<String> userOrganizationIds = userOrganizationService.findAllUserIDsRecursion(organizationID);
+
+                //部门下的ID列表去除已授权的id
+                List<String> intersectionIds = new ArrayList<>();
+
+                for (String userOrganizationId : userOrganizationIds) {
+                    if (!userRoleIds.contains(userOrganizationId)) {
+                        intersectionIds.add(userOrganizationId);
+                    }
+                }
+
+                //根据交集ID列表返回未授权用户列表
+                List<User> users = userDao.findByIdIn(intersectionIds);
+                outUserVOs = JpaListPO2VO.convert(users, OutUserVO.class);
+            }
+        } catch (Exception e) {
+            outUserVOs = null;
+            logger.error("获取未授权用户列表失败!", e);
+        }
+        return outUserVOs;
     }
 
     /**
