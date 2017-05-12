348dcbc0f2dd38ef9266c1eac0b1079b86260997
sdevil507 <sdevil507@163.com>
2017-05-12 15:04:19 +0800

    将微服务接口部分脱离于Controller

---
+++ admin-service/src/main/java/com/bizdata/feign/ResourceFeign.java
@@ -0,0 +1,41 @@
+package com.bizdata.feign;
+
+import com.bizdata.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 提供资源相关微服务接口实现
+ * <p>
+ * Created by sdevil507 on 2017/5/12.
+ */
+@RestController
+public class ResourceFeign {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    /**
+     * 获取登录用户授权访问的URL列表
+     *
+     * @return List<String>
+     */
+    @RequestMapping(value = "/listAuthUrl", method = RequestMethod.POST)
+    public List<String> listAuthUrl(String userID) {
+        return resourceService.listAuthUrl(userID);
+    }
+
+    /**
+     * 获取全部资源配置url列表
+     *
+     * @return List<String>
+     */
+    @RequestMapping(value = "/listResourceUrl", method = RequestMethod.POST)
+    List<String> listResourceUrl() {
+        return resourceService.listResourceUrl();
+    }
+}
